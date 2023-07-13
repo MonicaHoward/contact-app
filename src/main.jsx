c3eea310ebce812312e5e9fad3ddbdb46d8e2f48
Monica <jay.m.howard@gmail.com>
2023-07-12 20:41:32 -0500

    added loaderData, child routes, and contact ui

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import  { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/root';
 
+import Root, { loader as rootLoader } from './routes/root';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
 
 const router = createBrowserRouter([
   {
     path:'/',
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />
+      }
+    ]
   },
+  
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
